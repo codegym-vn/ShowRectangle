f8e3b904227ea0923a8050ada7e645daa028213d
tungnk_2711 <tungdemo@gmail.com>
2018-01-29 22:52:15 +0700

    init project Rectangle

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nhập vào chiều rộng hình chữ nhật
+            Console.Write("Enter the width of the rectangle : ");
+            double width = double.Parse(Console.ReadLine());
+
+            //Nhập vào chiều cao hình chữ nhật
+            Console.Write("Enter the height of the rectangle : ");
+            double height = double.Parse(Console.ReadLine());
+
+            //Khởi tạo đối tượng của lớp Rectangle
+            Rectangle rectangleObject = new Rectangle(height, width);
+
+            //Gọi phương thức hiển thị hình chữ nhật
+            rectangleObject.Display();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Rectangle
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double GetPerimeter()
+        {
+            return (Height + Width) * 2.0;
+        }
+
+        public double GetArea()
+        {
+            return Height * Width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("The width of the rectangle is: {0}", Width);
+            Console.WriteLine("The height of the rectangle is: {0}", Height);
+            Console.WriteLine("The perimeter of the rectangle is: {0}", GetPerimeter());
+            Console.WriteLine("The area of the rectangle is: {0}", GetArea());
+        }
+    }
+
+}
